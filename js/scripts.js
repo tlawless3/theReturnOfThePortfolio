6086702bdcbbed6a32ca5d3493b6ee47fcd06509
Thomas Lawless <tlawless94@gmail.com>
2017-11-06 14:47:40 -0600

    fix display bugs

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,9 @@ $(".portfolioLink").click(function(){
     left: "150%",
   }, 500);
   setTimeout(function(){
+    $(".navbarPortfolio").css("display", "");
     setTimeout(function(){
-      $(".navbarPortfolio").css("overflow", "visible");
+      $(".centerText").css("display", "none");
     }, 500);
     $(".portfolioContent").animate({
       right: "0%",
@@ -15,6 +16,11 @@ $(".portfolioLink").click(function(){
       left: "2.5%",
     }, 500);
   }, 500);
+});
+
+//TODO add code that shuffles the contact Link in and out
+$(".contactLink").click(function(){
+
 });
 
 //Portfolio Page
@@ -26,9 +32,10 @@ $("#homeButtonPortfolio").click(function(){
     left: "100%",
   }, 500);
   setTimeout(function(){
+    $(".centerText").css("display", "");
     setTimeout(function(){
       $(".centerText").css("position", "static");
-      $(".navbarPortfolio").css("overflow", "hidden");
+      $(".navbarPortfolio").css("display", "none");
     }, 500);
     $(".centerText").animate({
       left: "28%",
