96438610f88234389793dffcab5ab7046e059b2f
Thomas Lawless <tlawless94@gmail.com>
2017-11-10 13:27:29 -0600

    add toggle between contact and portfolio page

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,7 @@ $(".contactLink").click(function(){
 });
 
 //Portfolio Page
+//cycles in home page
 $("#homeButtonPortfolio").click(function(){
   $(".portfolioContent").animate({
     right: "100%",
@@ -49,7 +50,55 @@ $("#homeButtonPortfolio").click(function(){
     $(".centerText").animate({
       left: "28%",
     }, 500);
-  }, 500)
+  }, 500);
 });
+//cycles in contact page
+$("#contactButtonPortfolio").click(function(){
+  $(".portfolioContent").animate({
+    right: "100%",
+  }, 500);
+  $(".navbarPortfolio").animate({
+    left: "100%",
+  }, 500);
+  setTimeout(function(){
+    $(".contactContent").toggle();
+    $(".navbarPortfolio").css("display", "none");
+    $(".contactContent").animate({
+      left: "0%",
+    }, 500);
+  }, 500);
+})
 
 //Concact Page
+//cycles in home page
+$("#homeButtonContacts").click(function(){
+  $(".contactContent").animate({
+    left: "100%",
+  }, 500);
+  setTimeout(function(){
+    $(".contactContent").toggle();
+    $(".centerText").toggle();
+    setTimeout(function(){
+      $(".centerText").css("position", "static");
+    }, 500);
+    $(".centerText").animate({
+      left: "28%",
+    }, 500);
+  }, 500);
+});
+//cycles in portfolio page
+$("#portfolioButtonContacts").click(function(){
+  $(".contactContent").animate({
+    left: "100%",
+  }, 500);
+  setTimeout(function(){
+    $(".navbarPortfolio").css("display", "");
+    $(".contactContent").toggle();
+    $(".portfolioContent").animate({
+      right: "0%",
+    }, 500);
+    $(".navbarPortfolio").animate({
+      left: "2.5%",
+    }, 500);
+  }, 500);
+});
